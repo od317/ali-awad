b9b3e145f187c45245731d168b245d8221334eac
osama drbas <osamadrbas31@gmail.com>
2025-04-15 13:22:13 +0300

    Added (store page cards) [CU-86c32x4g0]

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
   images: {
-    domains: ["plus.unsplash.com","images.unsplash.com","localhost","upload.wikimedia.org"], // Allow this domain
+    domains: ["plus.unsplash.com","images.unsplash.com","localhost","upload.wikimedia.org"], // Allow these domains
   },
 };
  
